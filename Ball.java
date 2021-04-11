09e2192da3035591bc479df6429de99fc803a57d
Polly <71594933+PollyDimieva@users.noreply.github.com>
2021-04-11 16:00:27 +0300

    Add files via upload

--- Ball.java
+++ Ball.java
@@ -2,11 +2,23 @@ package lists_stack_queue;
 
 import java.awt.Color;
 
- class Ball {
+ class Ball implements Comparable<Ball>{
 	int x = 0;
 	int y = 0; // Current ball position
 	int dx = 2; // Increment on ball's x-coordinate
 	int dy = 2; // Increment on ball's y-coordinate
-	int radius = 5; // Ball radius
+//	int radius = 5; // Ball radius
+	
+	int radius = (int)(Math.random()*19 +2);
 	Color color = new Color((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
+	
+	public int compareTo(Ball a) {
+		if(radius - a.radius>0) {
+			return 1;
+		}else if(radius - a.radius<0) {
+			return -1;
+		}else {
+			return 0;
+		}
+	}
 }
--- BallPanel.java
+++ BallPanel.java
@@ -2,6 +2,9 @@ package lists_stack_queue;
 
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 import javax.swing.Timer;
 import java.awt.*;
 import javax.swing.*;
@@ -11,7 +14,10 @@ import javax.swing.JPanel;
 
 class BallPanel extends JPanel {
 	private int delay = 10;
-	private ArrayList<Ball> list = new ArrayList<Ball>();
+//	private ArrayList<Ball> list = new ArrayList<Ball>();
+	
+	private PriorityQueue<Ball> queue = new PriorityQueue<Ball>(Collections.reverseOrder()); //nai-golqmoto topche shte e nai-otpred
+	
 // Create a timer with the initial delay
 	protected Timer timer = new Timer(delay, new TimerListener());
 
@@ -24,102 +30,122 @@ class BallPanel extends JPanel {
 
 	public BallPanel() {
 		timer.start();
-		this.addMouseListener(new MouseListener(){
+		this.addMouseListener(new MouseListener() {
 			public void mouseClicked(MouseEvent e) {
 				int x = e.getX();
 				int y = e.getY();
-			    for (int j = 0 ; j < list.size(); j++) {
-					Ball ball = (Ball) list.get(j); 
-					double distance = BallPanel.distance(x,y,ball.x,ball.y);
-					if(ball.radius>=distance) {
-						list.remove(ball);
+				//for (int j = 0; j < list.size(); j++) {
+				//	Ball ball = (Ball) list.get(j);
+						for(Ball ball : queue) {
+					
+					double distance = BallPanel.distance(x, y, ball.x, ball.y);
+					if (ball.radius >= distance) { // toest e cuknato vutre v topcheto
+						//list.remove(ball); // moga da mahna element ot arraylista po vreme na obhojdaneto
+						queue.remove(ball);
 						break;
 					}
-					}
+				}
 			}
 
 			@Override
 			public void mousePressed(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 
 			@Override
 			public void mouseExited(MouseEvent e) {
 				// TODO Auto-generated method stub
-				
-			}
 
-			
+			}
 
 		});
 	}
 
 	public void add() {
-		list.add(new Ball());
+		//list.add(new Ball());
+		queue.offer(new Ball());	
+		System.out.println("Queue ");
+		for(Ball ball:queue) {
+			System.out.println(ball.radius);
+		}
+	
 	}
 
 	public void subtract() {
-		if (list.size() > 0)
-			list.remove(list.size() - 1); // Remove the last ball
+//		if (list.size() > 0)
+//			list.remove(list.size() - 1); // Remove the last ball
+		
+		if(queue.size()>0) {
+			queue.poll(); //vzimame topkata na purva pozizicq(nai-golqmata)
+			System.out.println("Queue ");
+		}
+		for(Ball ball:queue) {
+			System.out.println(ball.radius);
+		}
 	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		for (int i = 0; i < list.size(); i++) {
-			Ball ball = (Ball) list.get(i); // Get a ball
+		for(Ball ball : queue) {
+	//	for (int i = 0; i < list.size(); i++) {
+			//Ball ball = (Ball) list.get(i); // Get a ball
 			g.setColor(ball.color); // Set ball color
 			// Check boundaries
-			
+
 			if (ball.x < 0 || ball.x > getWidth())
 				ball.dx = -ball.dx;
 			if (ball.y < 0 || ball.y > getHeight())
 				ball.dy = -ball.dy;
-			
+
 			// Adjust ball position
-			
+
 			ball.x += ball.dx;
 			ball.y += ball.dy;
 			g.fillOval(ball.x - ball.radius, ball.y - ball.radius, ball.radius * 2, ball.radius * 2);
-	
+
+		}
+		ArrayList<Ball> ballsToRemove = new ArrayList<>();
+	//	for (int i = 0; i < list.size(); i++) { // obhojda vsichki topcheta
+	//		Ball ball1 = (Ball) list.get(i); // Get a ball
+		for(Ball ball1 : queue) {
+if(ballsToRemove.contains(ball1)==false) {
+			//for (int j = i + 1; j < list.size(); j++) { // obhojda vsichki topcheta sled tova v momenta(j=i+1)
+				//Ball ball2 = (Ball) list.get(j); // Get a second ball
+			
+			for(Ball ball2 : queue) {
+				if(ball2!=ball1) {
+				int radiusSum = ball1.radius + ball2.radius;
+				double distance = BallPanel.distance(ball1.x, ball1.y, ball2.x, ball2.y);
+				if (radiusSum >= distance) { // toest se presichat
+					ballsToRemove.add(ball2);
+					ball1.radius += ball2.radius;
+				}
 			}
-		ArrayList<Ball> ballsToRemove =new ArrayList<>();
-		for(int i = 0; i< list.size(); i++) {
-		Ball ball1 = (Ball) list.get(i); // Get a ball
-		
-		
-	    for (int j = i+1 ; j < list.size(); j++) {
-		Ball ball2 = (Ball) list.get(j); // Get a second ball
-		
-		int radiusSum = ball1.radius+ball2.radius;
-		double distance = BallPanel.distance(ball1.x,ball1.y,ball2.x,ball2.y);
-		if(radiusSum>=distance) {
-			ballsToRemove.add(ball2);
-			ball1.radius+=ball2.radius;
+}}
 		}
-		}
+		//list.removeAll(ballsToRemove); // premahvame vsichki topcheta ot arraylista ballsToRemove
+		queue.removeAll(ballsToRemove);
 	}
-		list.removeAll(ballsToRemove);
-}
-	
-		
-	public static double distance(int x1, int y1, int x2, int y2) {
-		return Math.sqrt(Math.pow(Math.abs(x1-x2), 2)+Math.pow(Math.abs(y1-y2),2));
+
+	public static double distance(int x1, int y1, int x2, int y2) { // razstoqnieto mejdu dve tochki s tehnite
+																	// koordinati
+		return Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2));
 	}
-	
+
 	public void suspend() {
 		timer.stop();
 	}
